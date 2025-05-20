6ad6e1415387f57814fbfcc5f6ccae015a74f1f8
Mokgabo-gif <bida24-172@thuto.bac.ac.bw>
2025-05-20 04:25:32 -0700

    Add script.js with personalized a welcome popup

---
+++ script.js
@@ -0,0 +1,14 @@
+window.load = function() {
+    let visitorName = localStorage.getItem("visitorName");
+    if (!visitorName) {
+        visitorName = prompt("Hi there! What's your name?");
+        if (visitorName) {
+            localStorage,setItem("visitorName", visitorName);
+        }
+    }
+}
+if (visitorName) {
+    alert("Welcome back to The Pink Whisk Bakery!" + visitorName + "!❤");
+} else {
+    alert("Wlcome to The Pink Whisk Bakery!❤");
+}
