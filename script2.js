a44c9cbba567e5298685ebcc60fee6e1292a7db3
Mokgabo-gif <bida24-172@thuto.bac.ac.bw>
2025-05-20 06:13:17 -0700

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById('themeToggle');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if (document.body.classList.contains('dark-mode')) {
+    toggleBtn.textContent = 'Switch to Light Mode';
+  } else {
+    toggleBtn.textContent = 'Switch to Dark Mode';
+  }
+});
